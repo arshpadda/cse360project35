a7e833ed92e6c9d7d5fb87eeafbc57a266aac002
Ulrich282 <alexanderloizou@gmail.com>
2016-03-28 15:50:14 -0700

    Test commit

---
+++ Dice Game/src/cse360project/Dice.java
@@ -0,0 +1,15 @@
+package cse360project;
+
+public class Dice {
+    
+    private int die1;   // Dice number 1
+    //make as many int variable as dice you need 
+             
+    public int getDie1() {
+       die1 = (int)(Math.random()*6) + 1;
+       return die1;
+    }
+    //make the same number of getDie method.
+ }
+
+ 
