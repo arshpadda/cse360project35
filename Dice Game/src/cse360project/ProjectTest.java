4bbe9d98b08926a20d6d1b792b1b26de6f928533
Alex Aguayo <akaguayo@asu.edu>
2016-04-15 16:33:15 -0700

    Updated

--- Dice Game/src/cse360project/ProjectTest.java
+++ Dice Game/src/cse360project/ProjectTest.java
@@ -15,7 +15,35 @@ public class ProjectTest {
 		
 		//Test health and weapons
 		assertEquals(batTest.getHealth(), 100);
+		assertEquals(batTest.getWeapon1(), 10);
+		assertEquals(batTest.getWeapon1(), 8);
+		assertEquals(batTest.getWeapon1(), 5);
 		
+		//Test health updating
+		batTest.updateHealth(75);
+		assertEquals(batTest.getHealth(), 75);
+		batTest.updateHealth(50);
+		assertEquals(batTest.getHealth(), 50);
+		batTest.updateHealth(100);
+		assertEquals(batTest.getHealth(), 100);
+	}
+	
+	@Test
+	public void testSuperman() {
+		
+		//Test health and weapons
+		assertEquals(superTest.getHealth(), 100);
+		assertEquals(superTest.getWeapon1(), 10);
+		assertEquals(superTest.getWeapon1(), 8);
+		assertEquals(superTest.getWeapon1(), 5);
+		
+		//Test health updating
+		superTest.updateHealth(75);
+		assertEquals(superTest.getHealth(), 75);
+		superTest.updateHealth(50);
+		assertEquals(superTest.getHealth(), 50);
+		superTest.updateHealth(100);
+		assertEquals(superTest.getHealth(), 100);
 	}
 
 }
