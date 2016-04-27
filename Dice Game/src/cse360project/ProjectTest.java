f39f5176cc975d41dc424ed82dfd2ac986c27ab2
Shweta Choudhary <shweta.choudhary@asu.edu>
2016-04-27 16:09:35 -0700

    TestUpdate

--- Dice Game/src/cse360project/ProjectTest.java
+++ Dice Game/src/cse360project/ProjectTest.java
@@ -1,107 +1,73 @@
 
-//package cse360project;
+package cse360project;
 
 /**
  * @author Team35
  * @version April 15, 2016
  */
-/*
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 
 public class ProjectTest {
 	
+	//initialize objects
 	Project game = new Project();
-	Batman batTest = new Batman();
-	Superman superTest = new Superman();
-
+	Character char1 = new Character(0);
+	Character char2 = new Character(1);
+	
 	@Test
 	public void testProject() {
-		
+		//test object is not null
 		assertNotNull(game);
 	}
 	
 	@Test
-	public void testBatman() {
-		
-		assertNotNull(batTest);
+	public void testChar1() {
+		//test object is not null
+		assertNotNull(char1);
 	}
 	
 	@Test
 	public void testDice() {
-		
+		//initialize  Dice Object
 		Dice die = new Dice();
 		assertNotNull(die);
 	}
 	
-	@Test
-	public void testSuperman() {
-		
-		assertNotNull(superTest);
-	}
-	
-	@Test
-	public void testBatmanHealth() {
 
-		assertEquals(batTest.getHealth(), 100);
+	
+	@Test
+	public void testChar1Health() {
+		//test to verify the health of character at the beginning of a game
+		assertEquals(char1.getHealth(), 100);
 	}
 	
 	@Test
-	public void testBatmanWeapon1() {
-		
-		assertEquals(batTest.getWeapon1(), 10);
+	public void testChar1Weapon1() {
+		//test the attack power of Weapon1
+		assertEquals(char1.getWeapon1(), 10);
 	}
 	
 	@Test
-	public void testBatmanWeapon2() {
-		
-		assertEquals(batTest.getWeapon2(), 8);
+	public void testChar1Weapon2() {
+		//test the attack power of Weapon2
+		assertEquals(char1.getWeapon2(), 8);
 	}
 
 	@Test
-	public void testBatmanWeapon3() {
-		
-		assertEquals(batTest.getWeapon3(), 6);
+	public void testChar1Weapon3() {
+		//test the attack power of Weapon3
+		assertEquals(char1.getWeapon3(), 6);
 	}
 	
 	@Test
-	public void testBatmanUpdateHealth() {
-		
-		batTest.updateHealth(75);
-		assertEquals(batTest.getHealth(), 75);
+	public void testChar1UpdateHealth() {
+		//test health updates during the game
+		char1.updateHealth(75);
+		assertEquals(char1.getHealth(), 75);
 	}
 	
-	
-	@Test
-	public void testSupermanHealth() {
-	
-		assertEquals(superTest.getHealth(), 100);
-	}
-	
-	@Test
-	public void testSupermanWeapon1() {
-		
-		assertEquals(superTest.getWeapon1(), 10);
-	}
-	
-	@Test
-	public void testSUpermanWeapon2() {
-		
-		assertEquals(batTest.getWeapon2(), 8);
-	}
 
-	@Test
-	public void testSupermanWeapon3() {
-		
-		assertEquals(superTest.getWeapon3(), 6);
-	}
-	
-	@Test
-	public void testSupermanUpdateHealth() {
-		
-		superTest.updateHealth(75);
-		assertEquals(superTest.getHealth(), 75);
-	}
 }
-*/
