69dac4e00fec4a562baeaf93cdc857fdb44bacdd
Shweta Choudhary <Shweta.Choudhary@asu.edu>
2016-04-29 16:31:33 -0700

    TestCases

--- Dice Game/src/cse360project/StatsTest.java
+++ Dice Game/src/cse360project/StatsTest.java
@@ -37,34 +37,7 @@ public class StatsTest
 		//initialize the object
 		Stats stats = new Stats(2,11);
 		assertEquals(0, stats.nameToInt("Batman"), 0);
-		//assertEquals(0, stats.nameToInt("Superman"), 1);
-	}
 
-	@Test
-	public void testAttackUpdate()
-	{
-		//initialize the object
-		Stats stats = new Stats(2,11);
-		
-		//call the method
-		//Batman wins, dealing 10 damage in a critical hit to the torso, and taking 5 damage.
-		stats.attackUpdate(0, 1, 10, 5, 1, 0, 11, 5, 12, 0);
-		
-		//check the effect on the statistics
-		assertEquals("Wins:		1\nLosses:		0\nDraws:		0\nTotal Turns:		1\nDamage Done:		10\nDamage Taken:	5\nHead Hits:		0\nBody Hits:		1\nLeg Hits:		0\nCritical Hits:		1\nMost Turns:		0\n", stats.printChar(0));
-	}
-
-	@Test
-	public void testMostTurns()
-	{
-		//initialize the object
-		Stats stats = new Stats(2,11);
-		
-		//call method
-		stats.mostTurns(0, 1, 10);
-		
-		//check the effect on the most turns statistic
-		assertEquals(stats.printChar(0), "Wins:		0\nLosses:		0\nDraws:		0\nTotal Turns:		0\nDamage Done:		0\nDamage Taken:	0\nHead Hits:		0\nBody Hits:		0\nLeg Hits:		0\nCritical Hits:		0\nMost Turns:		10\n");
 	}
 
 
