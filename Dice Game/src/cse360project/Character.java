f2f9de16022490dc0017bb9c302ac9e57834e893
Arsh Padda <apadda@asu.edu>
2016-04-29 10:46:22 -0700

    Documentation.

--- Dice Game/src/cse360project/Character.java
+++ Dice Game/src/cse360project/Character.java
@@ -9,11 +9,11 @@ import javax.swing.*;
  */
 
 public class Character {
-	int healthBar = 100;
-	int weapon1 = 10;
-	int weapon2 = 8;
-	int weapon3 = 6;
-	int hero;
+	int healthBar = 100;	// Health of the character when the battle begins	
+	int weapon1 = 10;		// Attack Power of weapon 1
+	int weapon2 = 8;		// Attack Power of weapon 2
+	int weapon3 = 6;		// Attack Power of weapon 3
+	int hero;				// hero Selector Number
 	
 	static String[] heroNames = {"Batman", "Superman", "Wonder Woman", "Flash", "Green Lantern", "Aquaman", "Cyborg", "Hawkman"};
 
@@ -38,47 +38,55 @@ public class Character {
 			new ImageIcon(PlayPanel.class.getResource("/cse360project/Aquaman.jpg")),
 			new ImageIcon(PlayPanel.class.getResource("/cse360project/Cyborg.jpg")),
 			new ImageIcon(PlayPanel.class.getResource("/cse360project/Hawkman.jpg")),
-			};
+			};		
+	// Images of the SuperHero 
 	
 	//Constructor that determines which character you are.
 	Character(int characterSelect){
 		hero = characterSelect;
 	}
 	
+	//Return the attack value of first weapon 
 	int getWeapon1(){
 		return weapon1;
 	}
 	
-	
+	//Return the attack value of second weapon
 	int getWeapon2(){
 		return weapon2;
 	}
 	
+	//Return the attack value of third weapon
 	int getWeapon3(){
 		return weapon3;
 	}
 	
-
+	// Update the health value of the health bar 
 	void updateHealth(int health){
 		healthBar = health;
 	}
 	
+	// Get the health value of the health bar
 	int getHealth(){
 		return healthBar;
 	}
 	
+	// Return the weapon name of the specified character 
 	String weaponName(int weaponNum){
 		return weapons[hero][weaponNum-1];
 	}
 	
+	// Return the hero name
 	String heroName(){
 		return heroNames[hero];
 	}
 	
+	// Return the pic of the hero selected
 	ImageIcon heroPic(){
 		return pictures[hero];
 	}
 	
+	// Return the number assigned to the hero 
 	int getHeroNum(){
 		return hero;
 	}
